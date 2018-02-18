70bc5d3f842bd3f1c3aacc586db21327789b2a17
Peter Kappus <peterk@peterkappus.com>
2018-02-18 23:03:59 +0000

    smoother flower forms.

--- scenes/flower.js
+++ scenes/flower.js
@@ -1,3 +1,27 @@
+
+function nonflower() {
+	clear();
+	background("#fff");
+	push();
+	translate(width / 2, height / 2);
+	noFill();
+	colorMode(HSB);
+	strokeWeight(0.2);
+
+	stroke(random(50),80,random(80));
+	beginShape();
+	var x =0;
+	while(x < width) {
+		curveVertex(cos(x)*width/5+x*cos(x),sin(x)*width/5);
+		x += 0.5
+	}
+	endShape();
+	pop();
+}
+
+
+
+
 function flower(x = 0.5) {
 	clear();
 	background("#fff");
@@ -10,13 +34,17 @@ function flower(x = 0.5) {
 	beginShape();
 
 	var rad = width*0.15;
-	var count = 4500;// random(100,5000);
+	var count = 2500;// random(100,5000);
 
-	var k = random(20);
-	var v = random(20);
-	var dr = 1.00018;
+	var factor = randFactor();
+	var k = Math.PI*2/(factor + random(0.001,0.01));//random(5);
+	console.log(factor);
+	//1/8,2/3,1/3,1/2,1,2,7,3,5,4,9
+	var factorB = random([1,3,5,6,7,10,1,1,1]);
+	var v = Math.PI*2/(factor * factorB + random(0.001,0.01));// + random(0.00001,0.0002);//random(1,5);
+	var dr = 1.00038;
 	var angle = 0;
-	var angleIncrement = 0.5;
+	var angleIncrement = 5;
 	//var v = k * factor;
 	console.log("k " + k + " v" + v + "count:" + count);
 	translate(width / 2, height / 2);
@@ -30,6 +58,10 @@ function flower(x = 0.5) {
 	pop();
 }
 
+function randFactor() {
+	return random([3,6,7,8,4,12,15,13,17,21]);
+	//return random([1/3,3/5,2,3,5,4,7,12,15,21]);
+}
 /*function mouseDragged() {
 	console.log(window.event.clientX);
 	var mouseX = window.event.clientX;
