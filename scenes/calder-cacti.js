dc537964bf4fb4f0bc9545bea7764b8f0e391cc1
Peter Kappus <peterk@peterkappus.com>
2019-09-30 17:27:32 +0100

    bw calder

--- scenes/calder-cacti.js
+++ scenes/calder-cacti.js
@@ -3,77 +3,33 @@
 // TODO: borrow from panton/dasCurve
 
 function cacti() {
-  var thickness = width/50;
+  //strokeJoin(MITRE);
+  strokeCap(ROUND);
   noFill();
-  strokeWeight(thickness);
+  //stroke(color(0,0,0,50));
   stroke("#000");
-  strokeJoin(ROUND);
-  
-  var points = [];
-
-  var y = height;
-  var x = width/2;
-  var min_dy = height / 50;
-  var max_dy = height * 0.5;
-  var min_dx = min_dy / 3;
-  var max_dx = max_dy * 0.8;
-  var max_rad = height/20;
-  var min_rad = height/40;
-
-  beginShape();
-  //begin at the bottom...
-  vertex(x,y);
-  var limit = random(height/2);
-  while(y - max_dy > limit) {
-      //up!
-      vertex(x,newY(x,y));
-
-      points.push(new Point(x,y));
-      
-      //over (left or right, less than up)
-      findNewPoint(x,y,dx());
-      vertex(newX(x,y),y);
-      
-      points.push(new Point(x,y));
-      
-      debug(x);
-      //thickness *= 0.9;
-      stroke(thickness);
-  }
-  endShape();
-  
-  //cactus top
-  fill("#fc0");
-  strokeWeight(width/200);
-  ellipse(x,y, min_rad + random(max_rad * ((height-y)/height)));
-  
-  //ellipse(p.getX(), p.getY(), height/4);
-  //return(new Point(x,y));
-  
+  angleMode(DEGREES);
+  branch(width/2,height,width/120,0,width/5,0);
 }
 
-//moveX
-function newX(x,y) {
-  
-}
-
-function newY(x,y) {
-  proposedY = y - min_dy + random(max_dy);
-  for(var j in points ) {
-    //check this one
-    if(this.overlaps(circs[j])) {
-      //oops overlapping one
-      return(true);
+function branch(x,y,thickness,angle,length, depth){
+  strokeWeight(thickness);
+  var newX = x - (sin(angle)*length);
+  var newY = y - (cos(angle)*length);
+  //debug(depth);
+  line(x,y,newX,newY);
+  da = 50;
+    if(depth < 9){
+      for(var i=0; i < 1 + random(2); i++) {
+        //debug(i);
+        branch(newX,newY,thickness * random(0.6,0.85),angle - (da/2) + random(0,da),length * random(0.5,0.9),depth+1);
+      }
+    } else {
+      flower(newX,newY);
     }
-  }
-  while(dist(x,y,x,proposedY)
 }
 
-function overlaps() {
+function flower(x,y) {
+  ellipse(x,y, width/200);
   
-}
-
-function findNewPoint(x,y,func){
-  proposedX = x + min_dx + random(max_dx) - (max_dx / 2);
-  proposedY = 
 }
