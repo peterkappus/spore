ddf4d1838f05e28320f25ed431ea3f85f702f959
Peter Kappus <62189+peterkappus@users.noreply.github.com>
2021-08-04 00:46:15 -0400

    tweaks

--- scenes/miami.js
+++ scenes/miami.js
@@ -15,27 +15,27 @@ function miami() {
 
 
   function fancyBox(x,y,w){
-    var colors = shuffle(["#fc0","#444","#359","#fa9","#fc0","#444","#359","#fa9","#fff"]);
+    var colors = shuffle(["#fc0","#444","#359","#fa9","#fff"]);
+    console.log(colors.join(' '));
+    // background square
     fill(colors.shift());
-    //strokeWeight(30);
     noStroke();
     rect(x, y, w, w);
     //now put something on top
     fill(colors.shift());
 
-    var type = random([1,2,3,4,5,6,7,8]);
+    var type = random([1,2,3,4,5,6,7,8,9]);
     
     switch(type) {
         case 1:
             beginShape();
+            vertex(x, y);
             //diag
             if(random() < 0.5) {
-                vertex(x,y);
                 vertex(x + w, y + w);
             }else {
                 //reverse orientation
-                vertex(x+w,y);
-                vertex(x, y);
+                vertex(x + w, y);
             }
             vertex(x, y+w);
             endShape();
@@ -44,7 +44,7 @@ function miami() {
             arc(x, y + (w/2), w, w, -PI/2, PI/2, PIE);
             break;
         case 3:
-            arc(x+w, y + (w/2), w, w, -PI/2, PI/2, PIE);
+            arc(x + w, y + (w/2), w, w, -PI/2, PI/2, PIE);
             break;
         case 4:
             arc(x, y, w * 2, w * 2, 0, PI/2, PIE);
@@ -53,13 +53,16 @@ function miami() {
             arc(x+w, y+w, w * 2, w * 2, PI, PI * 2, PIE);
             break;
         case 6:
-            rect(x,y,w/2,w);
+            rect(x, y, w/2, w);
             break;
         case 7:
             rect(x,y + (w/2), w, w / 2);
             break;
         case 8:
             arc(x+w, y + (w/2), w, w, PI/2, 3/2 * PI, PIE);
+            break;
+        case 9:
+            ellipse(x + (w/2), y + (w/2), w, w);
             break;
       }
     
